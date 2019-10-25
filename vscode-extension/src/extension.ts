292f561c670ed614855a91a0ee2428710fa7bf2b
Chris Wingler <chriswingler1986@gmail.com>
2019-10-24 18:55:46 -0700

    cleanup / format

--- vscode-extension/src/extension.ts
+++ vscode-extension/src/extension.ts
@@ -1,22 +1,22 @@
 import * as vscode from 'vscode';
 
-const myStatusBarItem: vscode.StatusBarItem = vscode.window.createStatusBarItem(
-  vscode.StatusBarAlignment.Right,
-  100
-);
-myStatusBarItem.text = "Clean Code Tips";
-
-
 export function activate({ subscriptions }: vscode.ExtensionContext) {
-  
-
   subscriptions.push(vscode.commands.registerCommand("onCommand:extension.displayTip", () => {
     // Display a message box to the user
     vscode.window.showInformationMessage(
       'Principles 🗽 > High Cohesion: Cohesion is the degree to which elements of a whole belong together. \nMethods and fields in a single class and classes of a component should have \nhigh cohesion. High cohesion in classes and components results in simpler, \nmore easily understandable code structure and design. \n'
       );
   }));
+
+  // Add status bar item
+  const myStatusBarItem: vscode.StatusBarItem = vscode.window.createStatusBarItem(
+    vscode.StatusBarAlignment.Right,
+    100
+  );
   
+  myStatusBarItem.text = "Clean Code Tips";
+  
+  // Make a tip show when clicking on status bar item
   myStatusBarItem.command = "onCommand:extension.displayTip";
 
 	// Display a message box to the user
@@ -27,6 +27,3 @@ export function activate({ subscriptions }: vscode.ExtensionContext) {
 	subscriptions.push(myStatusBarItem);
 	myStatusBarItem.show();
 }
-
-// this method is called when your extension is deactivated
-export function deactivate() {}
